e305990fd99a71fd5fd861a0850c65f7e7bcbb62
n3k <n3k1990@gmail.com>
2022-10-02 20:22:14 -0300

    added code templates folder

--- code_templates/rust/freestanding_binary/target/debug/deps/freestanding_binary-43dede4a943da1c0.d
+++
@@ -1,5 +0,0 @@
-/tmp/test_code_for_fun/code_templates/rust/freestanding_binary/target/debug/deps/freestanding_binary-43dede4a943da1c0.rmeta: src/main.rs
-
-/tmp/test_code_for_fun/code_templates/rust/freestanding_binary/target/debug/deps/freestanding_binary-43dede4a943da1c0.d: src/main.rs
-
-src/main.rs:
