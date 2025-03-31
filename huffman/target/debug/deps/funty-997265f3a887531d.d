a457ea57c0fc3cdbbb439769c6c4b4b12cbb7f4c
n3k <n3k1990@gmail.com>
2025-03-31 00:56:45 -0400

    works

--- huffman/target/debug/deps/funty-997265f3a887531d.d
+++
@@ -1,7 +0,0 @@
-/home/n3k/test_code_for_fun/huffman/target/debug/deps/libfunty-997265f3a887531d.rmeta: /home/n3k/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/funty-2.0.0/src/lib.rs
-
-/home/n3k/test_code_for_fun/huffman/target/debug/deps/libfunty-997265f3a887531d.rlib: /home/n3k/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/funty-2.0.0/src/lib.rs
-
-/home/n3k/test_code_for_fun/huffman/target/debug/deps/funty-997265f3a887531d.d: /home/n3k/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/funty-2.0.0/src/lib.rs
-
-/home/n3k/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/funty-2.0.0/src/lib.rs:
--- huffman/target/debug/deps/huffman-0a426ff8a84a0802.d
+++
@@ -1,5 +0,0 @@
-/home/n3k/test_code_for_fun/huffman/target/debug/deps/huffman-0a426ff8a84a0802: src/lib.rs
-
-/home/n3k/test_code_for_fun/huffman/target/debug/deps/huffman-0a426ff8a84a0802.d: src/lib.rs
-
-src/lib.rs:
